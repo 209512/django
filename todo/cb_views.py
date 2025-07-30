01c4fb01e4302eae2cbef9b11d21cf27e577ba63
209512 <209512mon@gmail.com>
2025-07-30 19:29:34 +0900

    ✨ Django 06 task
    Todo List App: Add Comment model, Use BootStrap5, Separate pagination functionality

--- todo/cb_views.py
+++ todo/cb_views.py
@@ -3,7 +3,10 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.http import HttpResponseRedirect, Http404
 from django.db.models import Q
-from todo.models import Todo
+from todo.models import Todo, Comment
+from django.core.paginator import Paginator
+from todo.forms import CommentForm, TodoForm, TodoUpdateForm
+from django.contrib import messages
 
 class TodoListView(LoginRequiredMixin, ListView):
     queryset = Todo.objects.all()
@@ -25,6 +28,10 @@ class TodoDetailView(LoginRequiredMixin, DetailView):
     model = Todo
     template_name = 'todo/todo_info.html'
 
+    def get_queryset(self):
+        # queryset = Todo.objects.all().prefetch_related('comments', 'comments__user')
+        return super().get_queryset().select_related('user').prefetch_related('comments__user')
+
     def get_object(self, queryset=None):
         obj = super().get_object(queryset)
         if obj.user != self.request.user and not self.request.user.is_superuser:
@@ -32,22 +39,29 @@ class TodoDetailView(LoginRequiredMixin, DetailView):
         return obj
 
     def get_context_data(self, **kwargs):
-        # context = {'todo': self.object.__dict__}
         context = super().get_context_data(**kwargs)
-        todo_dict = self.object.__dict__.copy()
-        todo_dict['user'] = self.object.user
-        context['todo'] = todo_dict
+
+        context['todo_dict'] = self.object.__dict__
+        context['comment_form'] = CommentForm()
+
+        comments = self.object.comments.all().order_by('-created_at')
+        paginator = Paginator(comments, 5)
+        page_number = self.request.GET.get('page', '1')
+        page_obj = paginator.get_page(page_number)
+        context['page_obj'] = page_obj
+
         return context
 
 class TodoCreateView(LoginRequiredMixin, CreateView):
     model = Todo
-    fields = ['title', 'description', 'start_date', 'end_date']
+    form_class = TodoForm
     template_name = 'todo/todo_create.html'
 
     def form_valid(self, form):
         self.object = form.save(commit=False)
         self.object.user = self.request.user
         self.object.save()
+        messages.success(self.request, '할 일이 생성되었습니다.')
         return HttpResponseRedirect(self.get_success_url())
 
     def get_success_url(self):
@@ -55,7 +69,7 @@ class TodoCreateView(LoginRequiredMixin, CreateView):
 
 class TodoUpdateView(LoginRequiredMixin, UpdateView):
     model = Todo
-    fields = ['title', 'description', 'start_date', 'end_date', 'is_completed']
+    form_class = TodoUpdateForm
     template_name = 'todo/todo_update.html'
 
     def get_object(self, queryset=None):
@@ -63,6 +77,10 @@ class TodoUpdateView(LoginRequiredMixin, UpdateView):
         if obj.user != self.request.user and not self.request.user.is_superuser:
             raise Http404('해당 Todo 수정 권한이 없습니다.')
         return obj
+
+    def form_valid(self, form):
+        messages.success(self.request, '할 일이 수정되었습니다.')
+        return super().form_valid(form)
 
     def get_success_url(self):
         return reverse_lazy('cbv_todo_info', kwargs={'pk': self.object.id})
@@ -76,5 +94,62 @@ class TodoDeleteView(LoginRequiredMixin, DeleteView):
             raise Http404('해당 Todo 삭제 권한이 없습니다.')
         return obj
 
+    def form_valid(self, form):
+        self.object.delete()
+        messages.success(self.request, '할 일이 삭제되었습니다.')
+        return HttpResponseRedirect(self.get_success_url())
+
     def get_success_url(self):
         return reverse_lazy('cbv_todo_list')
+
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    fields = ['message']
+    pk_url_kwarg = 'todo_id'
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.todo = Todo.objects.get(id=self.kwargs['todo_id'])
+        self.object.save()
+        messages.success(self.request, '댓글이 추가되었습니다.')
+        return HttpResponseRedirect(self.get_success_url())
+
+    def get_success_url(self):
+        return reverse_lazy('cbv_todo_info', kwargs={'pk': self.kwargs['todo_id']})
+
+class CommentUpdateView(LoginRequiredMixin, UpdateView):
+    model = Comment
+    fields = ['message']
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        if obj.user != self.request.user and not self.request.user.is_superuser:
+            raise Http404('해당 댓글 수정 권한이 없습니다.')
+        return obj
+
+    def form_valid(self, form):
+        messages.success(self.request, '댓글이 수정되었습니다.')
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('cbv_todo_info', kwargs={'pk': self.object.todo_id})
+
+class CommentDeleteView(LoginRequiredMixin, DeleteView):
+    model = Comment
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        if obj.user != self.request.user and not self.request.user.is_superuser:
+            raise Http404('해당 댓글 삭제 권한이 없습니다.')
+        return obj
+
+    def form_valid(self, form):
+        self.object.delete()
+        messages.success(self.request, '댓글이 삭제되었습니다.')
+        return HttpResponseRedirect(self.get_success_url())
+
+    def get_success_url(self):
+        # return reverse_lazy('cbv_todo_list') = 기본 요구사항
+        # 댓글 삭제 후 Todo 상세 페이지로 돌아가며 댓글 섹션으로 스크롤
+        return reverse_lazy('cbv_todo_info', kwargs={'pk': self.object.todo.id}) + '#comment_wrapper'
--- todo/forms.py
+++ todo/forms.py
@@ -1,12 +1,42 @@
 from django import forms
-from todo.models import Todo
+from todo.models import Todo, Comment
 
 class TodoForm(forms.ModelForm):
     class Meta:
         model = Todo
-        fields = ['title', 'description','start_date', 'end_date']
+        fields = ['title', 'description','start_date', 'end_date', 'is_completed']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': '할 일 제목'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 5, 'placeholder': '자세한 내용'}),
+            'start_date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'end_date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'is_completed': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+        }
 
 class TodoUpdateForm(forms.ModelForm):
     class Meta:
         model = Todo
         fields = ['title', 'description','start_date', 'end_date', 'is_completed']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
+            'start_date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'end_date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'is_completed': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+        }
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['message']
+        labels = {
+            'message': '내용',
+        }
+        widgets = {
+            'message': forms.Textarea(attrs={
+                'rows': 3,
+                'cols': 40,
+                'class': 'form-control',
+                'placeholder': '댓글 내용을 입력해주세요,'
+            }),
+        }
